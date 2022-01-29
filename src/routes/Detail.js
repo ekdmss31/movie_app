3ebcde91c978cc8464160c37b7b4aa6009acd1db
ekdmss31 <ekdmss31@naver.com>
2022-01-29 23:09:49 +0900

    UPDATE: Detail

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,4 +1,24 @@
+import { useParams, useEffect, Link } from "react-router-dom";
+
 function Detail() {
-  return <h1>Detail</h1>;
+  //const x = useParams();
+  const { id } = useParams();
+
+  //console.log(id);
+  const getMovie = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    console.log(json);
+  };
+  useEffect(() => {
+    getMovie();
+  }, []);
+  return (
+    <div>
+      <Link to="/">Home</Link>
+      <h1>Detail</h1>
+    </div>
+  );
 }
 export default Detail;
